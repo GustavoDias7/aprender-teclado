876c56cdabcc158ea8ecbde2a2152b90bee62f0e
Gustavo <gustavo7dias@gmail.com>
2021-09-14 10:50:09 -0300

    Create faq and accordions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,34 @@ function initSidebar({
 }) {
 
 }
-initLinkUnderline({ linkSelector: '.link' });
+function accordion({ selectorName, onlyOneOpen = false }) {
+  const $accordions = document.querySelectorAll(selectorName);
+  let lastOpen = 0;
+  let setLastOpen = (newValue) => {
+    lastOpen = newValue;
+  };
+
+  $accordions.forEach((accordion, index) => {
+    accordion.addEventListener('click', (event) => {
+      event.currentTarget.toggleAttribute('show');
+      if (onlyOneOpen === false) return;
+      let isDifferent = $accordions[lastOpen] != accordion;
+      if (isDifferent) {
+        $accordions[lastOpen].removeAttribute('show');
+        setLastOpen(index);
+      }
+    })
+  })
+}
+
+initLinkUnderline({ 
+  linkSelector: '.link' 
+});
 initSidebar({
   hamburgerSelector: '',
   sidebarContainer: '',
 })
+accordion({
+  selectorName: '[data-only]',
+  onlyOneOpen: true,
+});
